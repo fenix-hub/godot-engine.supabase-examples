93a1c400a70d54b9bd5b1f39d329d75c6766950c
fenix-hub <n.santilio97@gmail.com>
2021-10-08 15:48:15 +0200

    update todo-list example

--- todo-list/scn/LogScreen/log_screen.gd
+++ todo-list/scn/LogScreen/log_screen.gd
@@ -9,30 +9,35 @@ var pwd : String
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Supabase.auth.connect("error", self, "_on_auth_error")
-	error_lbl.hide()
+    Supabase.auth.connect("error", self, "_on_auth_error")
+    error_lbl.hide()
 
 func _on_auth_error(error : SupabaseAuthError):
-	print(error)
-	print_error(str(error))
-	match error.type :
-		"invalid_grant":
-			sign_up(mail ,pwd)
+    get_tree().call_group("loading_scene", "set_loading")
+    print(error)
+    print_error(str(error))
+    match error.type :
+        "invalid_grant":
+            sign_up(mail ,pwd)
 
 func sign_in(email : String, password : String):
-	Supabase.auth.sign_in(email, password)
-	error_lbl.hide()
+    Supabase.auth.sign_in(email, password)
+    error_lbl.hide()
 
 func sign_up(email : String, password : String):
-	Supabase.auth.sign_up(email, password)
-	error_lbl.hide()
+    Supabase.auth.sign_up(email, password)
+    error_lbl.hide()
+    get_tree().call_group("loading_scene", "set_loading", true)
 
 func _on_SignInBtn_pressed():
-	mail = user_email.get_text()
-	pwd = user_passwrd.get_text()
-	if mail.length() > 1 and pwd.length() > 1:
-		 sign_in(mail, pwd)
+    mail = user_email.get_text()
+    pwd = user_passwrd.get_text()
+    if mail.length() > 1 and pwd.length() > 1:
+        sign_in(mail, pwd)
+        get_tree().call_group("loading_scene", "set_loading", true)
+    else:
+        print_error("'mail' and 'password' fields must be filled.")
 
 func print_error(text : String):
-	error_lbl.show()
-	error_lbl.set_text(text)
+    error_lbl.show()
+    error_lbl.set_text(text)
